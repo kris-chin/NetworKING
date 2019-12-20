d1397dc4b680216964903e303109ce6db0fef3b2
Krikrifreeze <krikrifreeze@gmail.com>
2019-12-20 15:25:12 -0800

    renamed some files and added TODO comment

--- src/flask/__init__.py
+++ src/flask/__init__.py
@@ -1,3 +1,4 @@
+#TODO: add DB Access from the prototype (needs to be drawn a bit)
 from flask import Flask
 
 app = Flask(__name__) #our app is a new Flask instance
@@ -6,7 +7,8 @@ app = Flask(__name__) #our app is a new Flask instance
 @app.route('/') #by decorating route() with ('/'), it binds any following functions with the '/' URL
 
 def hello_world(): #for example, '/' is bound to hello_world
-    return "Hello World"
+    #the tutorials say to do all of this in an HTML template and not literally just returning a string of HTML code, but hey.
+    return '<a href="http://google.com/"> Click this to go to google </a>'
 
 #this only runs if the file was run as a script
 if __name__ == '__main__':
