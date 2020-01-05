35fa879e3a95ccb216b30e732890ad942577e82c
Krikrifreeze <krikrifreeze@gmail.com>
2020-01-04 20:42:11 -0800

    Added String Functions for Objects and Getters for Graph Object

--- prototype2/Graph.py
+++ prototype2/Graph.py
@@ -2,14 +2,19 @@ class Classification:
     def __init__(self, name, color):
         self.name = name #STRING
         self.color = color #STRING
+    def __str__(self):
+        return self.name
 
 class Vertex:
-    def __init__(self, name, type, health, shape, notes):
+    def __init__(self, name, type, health, shape = 'o', notes = 'None'):
         self.name = name #STRING
         self.type = type #CLASSIFICATION OBJECT
         self.health = health #INT
         self.shape = shape #CHAR
         self.notes = notes #STRING
+    
+    def __str__(self):
+        return self.name
     
     #Updates the vertex by just replacing it's data with another vertex's
     #If you're just changing one value, you pass in a Vertex with only one different value
@@ -20,12 +25,26 @@ class Vertex:
         self.shape = newVertex.shape
         self.notes = newVertex.notes
 
+    def PrintDetails(self):
+        print("Name: '" + self.name + "'")
+        print("Type: '" + str(self.type) + "'")
+        print("Health: " + str(self.health))
+        print("Shape: '" + self.shape + "'")
+        if self.notes != "None":
+            print("Notes: " + self.notes)
+
 class Edge:
-    def __init__(self, vertex1, vertex2, color, size, style):
-        self.vertices = (vertex1,vertex2) #2-TUPLE OF VERTEX OBJECTS
+    def __init__(self, vertices, color = "Black", size = 1, style = 'solid'):
+        self.vertices = vertices #2-TUPLE OF VERTEX OBJECTS
         self.color = color #STRING
         self.size = size #INT, THICKNESS
         self.style = style #STRING, STYLING
+
+    def __getitem__(self, key): #used for accessing either Vertex1 or Vertex2
+        return self.vertices[key]
+
+    def __str__(self):
+        return "(" + str(self.vertices[0]) + ", " + str(self.vertices[1]) + ")"
     
     #Same as Vertex.Update()
     def Update(self, newEdge):
@@ -38,12 +57,38 @@ class Graph:
     def __init__(self, vertices, edges):
         self.vertices = vertices #LIST OF VERTEX OBJECTS
         self.edges = edges #LIST OF EDGE OBJECTS
+
+    def __str__(self):
+        vertex_strings = list(str(Vertex) for Vertex in self.vertices)
+        edge_strings = list(str(Edge) for Edge in self.edges)
+        return "Vertices: " + str(vertex_strings) + "\nEdges: " + str(edge_strings)
+
     def AddEdge(self, edge): self.edges.append(edge)
     def AddVertex(self, vertex): self.vertices.append(vertex)
 
+    #Returns the Vertex Object of matching name string
     def GetVertex(self, vertex_name):
         for vertex in self.vertices:
             if vertex.name == vertex_name:
                 return vertex
         #if you're here, there is no vertex of the given name
         return None
+
+    def GetVertices(self):
+        return self.vertices
+    def GetEdges(self):
+        return self.edges
+            
+    #Returns a list of all neighboring Vertices
+    def GetNeighbors(self,vertex):
+        NeighborList = []
+
+        for Edge in self.edges:
+            if (Edge[0] == vertex):
+                if (Edge[1] not in NeighborList): #if the other node is not already in the list
+                    NeighborList.append(Edge[1]) #append it
+            elif (Edge[1] == vertex):
+                if (Edge[0] not in NeighborList): #if the other node is not already in the list
+                    NeighborList.append(Edge[0]) #append it
+        
+        return NeighborList
