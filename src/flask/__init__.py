ce8ac444750199355532257c591119f301f862a1
Krikrifreeze <krikrifreeze@gmail.com>
2020-01-15 19:04:06 -0800

    Small GET/POST and TEMPLATE tests

--- src/flask/__init__.py
+++ src/flask/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from .model import *
+from flask import Flask, render_template, request
+#from model import *
 
 app = Flask(__name__) #our app is a new Flask instance
 
@@ -9,6 +9,15 @@ app = Flask(__name__) #our app is a new Flask instance
 def hello_world(): #for example, '/' is bound to hello_world
     #the tutorials say to do all of this in an HTML template and not literally just returning a string of HTML code, but hey.
     return '<a href="http://google.com/"> Click this to go to google </a>'
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user2 = {'username' : 'POSTIE'}
+        return render_template('index.html', title ='THE GANG 2', user=user2)
+    else:
+        user = {'username' : 'dog'} #this is a json
+        return render_template('index.html', title='THE GANG', user=user)
 
 #this only runs if the file was run as a script
 if __name__ == '__main__':
